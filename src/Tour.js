9300d8e7d9b68af4493f36700a93cbd48f90a245
hritik <86347286+hritikd3@users.noreply.github.com>
2022-02-08 00:03:42 +0530

    Initial commit

---
+++ src/Tour.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const Tour = ({ id, name, info, image, price, removeTour }) => {
+  const [more, setMore] = useState(false);
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>{more ? info : `${info.substring(0, 200)}...`}</p>
+        <button onClick={() => setMore(!more)}>
+          {more ? "show less" : "read more"}
+        </button>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          Not Interested in this place
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default Tour;
